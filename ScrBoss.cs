3845a7cbecd199e1f70d2b6f04a3c78753a443f8
OmarMuAr3099 <77741710+OmarMuAr3099@users.noreply.github.com>
2021-01-20 18:29:40 +0100

    Add files via upload

---
+++ ScrBoss.cs
@@ -0,0 +1,135 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+/// <summary>
+/// ----------------------------------------------------------------------------------
+/// DESCRIPCIÓ
+///         Controla comportament Boss
+/// AUTOR:  Omar Muñoz
+/// VERSIÓ: 1.0
+/// ----------------------------------------------------------------------------------
+/// </summary>
+
+public class ScrBoss : MonoBehaviour
+{
+    public float vel = -0.5f;
+    Rigidbody2D rb;
+    [SerializeField] GameObject explosio;
+    Collider2D col;
+    [SerializeField] Renderer r;
+
+    Animator ani;
+
+    bool up;
+    bool down;
+    float TimeRun = 4f;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        col = GetComponent<Collider2D>();
+        col.enabled = false;
+        rb = GetComponent<Rigidbody2D>();
+        ani = GetComponentInChildren<Animator>();
+        down = true;
+        up = false;
+    }
+    private void FixedUpdate()
+    {
+        rb.velocity = new Vector2(0, vel);
+        Moviment();
+    }
+    void Update()
+    {
+
+        if (ScrControlGame.EsVisibleDesde(r, Camera.main))  // Només si el pop està a pantalla
+        {
+            col.enabled = true;
+        }
+
+        if (ScrCollision.vidaBoss > 75)
+        {
+            ScrNPCShot.cadenciaMax = 3;
+        }
+        if (ScrCollision.vidaBoss <= 75 && ScrCollision.vidaBoss > 30)
+        {
+            ScrNPCShot.cadenciaMax = 2;
+        }
+        if (ScrCollision.vidaBoss <= 30)
+        {
+            ScrNPCShot.cadenciaMax = 1;
+            ScrApuntar.finalFase = true;
+        }
+    }
+
+    void Destruccio() // indica com es destrueix l'objecte
+    {
+        if (explosio) Instantiate(explosio, transform.position, transform.rotation);
+        Destroy(gameObject);
+    }
+
+    void Moviment()
+    {
+        if (up == true)
+        {
+            if (ScrCollision.vidaBoss > 75)
+            {
+                vel = 0.5f;
+                TimeRun -= Time.deltaTime;
+            }
+            if (ScrCollision.vidaBoss <= 75 && ScrCollision.vidaBoss > 30)
+            {
+                vel = 1f;
+                TimeRun -= Time.deltaTime;
+            }
+            if (ScrCollision.vidaBoss <= 30)
+            {
+                vel = 1.5f;
+                TimeRun -= Time.deltaTime;
+            }
+        }
+        if (down == true)
+        {
+            if (ScrCollision.vidaBoss > 75)
+            {
+                vel = -0.5f;
+                TimeRun -= Time.deltaTime;
+            }
+            if (ScrCollision.vidaBoss <= 75 && ScrCollision.vidaBoss > 30)
+            {
+                vel = -1f;
+                TimeRun -= Time.deltaTime;
+            }
+            if (ScrCollision.vidaBoss <= 30)
+            {
+                vel = -1.5f;
+                TimeRun -= Time.deltaTime;
+            }
+        }
+        if (TimeRun <= 0)
+        {
+            if (ScrCollision.vidaBoss > 75)
+            {
+                TimeRun = 4;
+                up = !up;
+                down = !down;
+            }
+            if (ScrCollision.vidaBoss <= 75 && ScrCollision.vidaBoss > 30)
+            {
+                TimeRun = 2;
+                up = !up;
+                down = !down;
+            }
+            if (ScrCollision.vidaBoss <= 30)
+            {
+                TimeRun = 1.5f;
+                up = !up;
+                down = !down;
+            }
+        }
+    }
+
+}
+
+
---
+++ ScrUIBOSS.cs
@@ -0,0 +1,64 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+
+/// <summary>
+/// ----------------------------------------------------------------------------------
+/// DESCRIPCIÓ: Script para controlar UI
+/// AUTOR:  Omar Muñoz
+/// VERSIÓ: 1.0
+/// ----------------------------------------------------------------------------------
+/// </summary>
+
+public class ScrUIBOSS : MonoBehaviour
+{
+
+    public Text tiempo;
+    public Text ronda;
+    public Image CorazonesVida;
+    public Image VidaBoss;
+    public GameObject Ronda;
+    public GameObject Perdiste;
+    float canvasTime;
+    public static float roundTime = 0;
+    public static float finalTime = 5;
+    public GameObject So;
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        roundTime = 0;
+        ScrCollision.vidaBoss = 100;
+        ScrCollision.vidaPlayer = 10;
+        Ronda.SetActive(true);
+        Perdiste.SetActive(false);
+        So.SetActive(true);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+        roundTime += Time.deltaTime;
+        tiempo.text = roundTime.ToString("0.0"); 
+        ronda.text = "Ronda Final" ;
+        CorazonesVida.fillAmount = (ScrCollision.vidaPlayer / 10); 
+        VidaBoss.fillAmount = (ScrCollision.vidaBoss / 100); 
+        canvasTime += Time.deltaTime;
+        if (canvasTime >= 2) Ronda.SetActive(false);
+        if (ScrCollision.vidaPlayer <= 0)
+        {
+            Perdiste.SetActive(true);
+            So.SetActive(false);
+        }
+        if (ScrCollision.vidaBoss <= 0)
+        {
+           
+           SceneManager.LoadScene("Winner");
+        }
+    }
+
+}
